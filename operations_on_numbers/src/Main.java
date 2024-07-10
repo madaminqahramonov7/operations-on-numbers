8f4720a76eb8252d6ae941052ed8fa50a48ea6dd
akhmatkulov <akhmatkulovs@gmail.com>
2024-07-10 17:26:16 +0500

    done ekub

--- operations_on_numbers/src/Main.java
+++ operations_on_numbers/src/Main.java
@@ -1,7 +1,21 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-
+        Scanner scanInt = new Scanner(System.in);
+        while (true) {
+            System.out.print("enter number a: ");
+            int a = scanInt.nextInt();
+            System.out.print("enter number b: ");
+            int b = scanInt.nextInt();
+            int ekub = a;
+            while (true) {
+                if (a % ekub == 0 && b % ekub == 0) {
+                    break;
+                }
+                ekub--;
+            }
+            System.out.println("ekub --> " + ekub);
+        }
     }
 }
