d725ef05a35a0551c0c7940b7c860bb2fb9d6b06
Khurshid Eshonkulov <khurshideshonkulov@Khurshids-MacBook-Pro.local>
2024-07-10 17:36:30 +0500

    boldim

--- operations_on_numbers/src/Main.java
+++ operations_on_numbers/src/Main.java
@@ -1,7 +1,25 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
     public static void main(String[] args) {
-
+        perfectNumbers(6);
+        perfectNumbers(28);
+        perfectNumbers(12);
+        perfectNumbers(496);
+        perfectNumbers(8128);
+        perfectNumbers(18);
     }
-}
+
+        public static void perfectNumbers(int n){
+            int summa = 0;
+            for(int i = 1; i < n; i++){
+                if(n % i == 0){
+                    summa+=i;
+                }
+            }
+            if(summa == n){
+                System.out.println("Perfect number");
+            }else{
+                System.out.println("Not a perfect number");
+            }
+        }
+    }
